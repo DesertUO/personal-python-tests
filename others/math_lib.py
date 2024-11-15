129414d83467e81fa056d4df3c3f67374d0c5ac6
DesertUO <51250040+DesertUO@users.noreply.github.com>
2024-11-15 17:26:17 -0500

    added some methods for reading Vec2, Vec3 and Polar elements

--- others/math_lib.py
+++ others/math_lib.py
@@ -1,5 +1,7 @@
 from math import cos, sin, atan2, tau
 
+from utils import sanatizeInputFloat
+
 # Just went full English from this part xD
 class Vec2:
     def __init__(self, coord_x: float, coord_y: float):
@@ -436,3 +438,22 @@ class Line:
             return f"{self.A}x + {self.B}y + {self.C} = 0"
         elif self.form == "point-slope":
             return f"y - {self.pass_point.y} = {self.m}(x - {self.pass_point.y})"
+
+def readVec2Console(msg: str = "Next you will enter the coordinates of the Vec2", msg_x: str = "Enter the value on the X axis: ", msg_y: str = "Enter the value on the Y axis: "):
+    print(msg)
+    _x = sanatizeInputFloat(msg_x)
+    _y = sanatizeInputFloat(msg_y)
+    return Vec2(_x, _y)
+
+def readVec3Console(msg: str = "Next you will enter the coordinates of the Vec3", msg_x: str = "Enter the value on the X axis: ", msg_y: str = "Enter the value on the Y axis: ", msg_z: str = "Enter the value on the Z axis: "):
+    print(msg)
+    _x = sanatizeInputFloat(msg_x)
+    _y = sanatizeInputFloat(msg_y)
+    _z = sanatizeInputFloat(msg_z)
+    return Vec2(_x, _y, _z)
+
+def readPolarConsole(msg: str = "Next you will enter the coordinates of the Polar", msg_r: str = "Enter the value on the X axis: ", msg_theta: str = "Enter the value on the Y axis: "):
+    print(msg)
+    _r = sanatizeInputFloat(msg_r)
+    _theta = sanatizeInputFloat(msg_theta)
+    return Polar(_r, _theta)
